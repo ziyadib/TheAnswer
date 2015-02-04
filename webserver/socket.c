9d456c0ddd46e4b72433fe9cf57ad34b8933e0eb
ziyadib <bader.ziyadi@etudiant.univ-lille1.fr>
2015-02-04 10:06:59 +0100

    reuseAddr fonctionnel

--- webserver/socket.c
+++ webserver/socket.c
@@ -13,6 +13,7 @@ int creer_serveur(int port){
 struct sockaddr_in saddr;
 
 int socket_serveur;
+int optval = 1;
 
 socket_serveur = socket(AF_INET,SOCK_STREAM, 0);
 
@@ -26,6 +27,9 @@ saddr.sin_family = AF_INET ; /* Socket ipv4 */
 saddr.sin_port = htons (port); /* Port d ’ écoute */
 saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
 
+if (setsockopt(socket_serveur, SOL_SOCKET , SO_REUSEADDR , &optval , sizeof( int )) == -1){
+perror ( " Can not set SO_REUSEADDR option " );
+}
     if(bind(socket_serveur,(struct sockaddr *)&saddr , sizeof(saddr)) == -1)
     {
             perror ( " bind socker_serveur " );
