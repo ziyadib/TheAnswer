cc133cd216fb66606b7b580de7794065a25bc1e5
ziyadib <bader.ziyadi@etudiant.univ-lille1.fr>
2015-02-18 09:22:10 +0100

    dernier version dispo

--- webserver/main.c
+++ webserver/main.c
@@ -15,13 +15,29 @@ void initialiser_signaux(void){
 	}
 }
 
+void filterString(char string[]){
+    unsigned int i;
+     char r= '\r';
+     char n ='\n';
+    for(i =0; i<strlen(string); i++){
+    
+    if(string[i] == r || string[i] == n ){
+        string[i]= '\0';
+    }
+    
+    }
+
+}
+
 int main(){
 	int clientfd, /*retfd,*/socket_serveur ;
 	unsigned int size_client;
-	char sms_client[1024];
+	 char sms_client[1024] ;
 	struct sockaddr_in client_addr;
 	char *serverName = "TheAnswer";
-	char *MESSAGE_BIENVENUE = "Bonjour, Welcome, Bienvenido, Bienvenito, ahlan wa sahlan, vous etes connecté au merveilleux serveur The Answer, tout vos desirs sont des ordres je me ferais un réel plaisir de vous servir et de pouvoir repondre à vos magnifiques questions avec la plus belle facon possible, c'est a dire avec un super smile :D, bonnne navigation et au plaisir de vous revoir tres bientot et tres souvent merveilleux utilisateur que vous etes\n";
+	char *retgets;
+	/*char *MESSAGE_BIENVENUE = "Bonjour, Welcome, Bienvenido, Bienvenito, ahlan wa sahlan, vous etes connecté au merveilleux serveur The Answer, tout vos desirs sont des ordres je me ferais un réel plaisir de vous servir et de pouvoir repondre à vos magnifiques questions avec la plus belle facon possible, c'est a dire avec un super smile :D, bonnne navigation et au plaisir de vous revoir tres bientot et tres souvent merveilleux utilisateur que vous etes\n";
+	*/
 	initialiser_signaux();
 	if((socket_serveur=creer_serveur(8080))==-1){
 		perror("pb creer_serveur");
@@ -45,24 +61,30 @@ int main(){
          }
          
 		/*envoie du sms de bienvenue */
-		if(write(clientfd,MESSAGE_BIENVENUE, strlen(MESSAGE_BIENVENUE))==-1){
+		/*if(write(clientfd,MESSAGE_BIENVENUE, strlen(MESSAGE_BIENVENUE))==-1){
 			perror("error welcome message");
 			return -1;
 		}
-		printf("jecris un sms de bienvenue\n");
-		while(1){
+		
+		printf("jecris un sms de bienvenue\n");*/
+		do{
+		
 		/*utilisation de fgets au lieu de read pour lire une ligne recu du client*/
-		    if(fgets(sms_client, 1024, open) == NULL){
+		    if((retgets =fgets(sms_client, 1024, open)) == NULL){
 		        perror("fgets error");
 		        return -1;
-		    }
+		        }
+		        printf("%s", sms_client);
+		        filterString(sms_client);
+		    }while(retgets != NULL || strlen(sms_client));
 			
-			printf("%s \n", sms_client);
+			
+		
+		printf("%s \n", sms_client);
 			if(fprintf(open, "<%s>  %s",serverName, sms_client)<=0){
 				perror("error perroquet fprintf");
 				return -1;
 			}
-		}
 		printf("client deconnecté\n");
 		exit(0);
 		
