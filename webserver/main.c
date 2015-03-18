bc9494d77f92eac6618e7a6a14afa864f2527d63
ziyadib <bader.ziyadi@etudiant.univ-lille1.fr>
2015-03-18 14:47:15 +0100

    tag url_or_404_clean

--- webserver/main.c
+++ webserver/main.c
@@ -24,7 +24,7 @@ char url[1024];
 
 void traitement_signal(int sig){
     waitpid(-1, NULL, WNOHANG);
-    printf("Signal%dreçu\n client deconnecté",sig);
+    printf("Signal%dreçu \n client deconnecté",sig);
 }
 
 void initialiser_signaux(void){
@@ -64,7 +64,7 @@ int parse_http_request(FILE *open,http_request *request){
     /* la lecture de la premiere ligne etait deja faite precedemment dans cette fonction */
     int ok = 1; //0=false, 1=true;
     char premiere_ligne[1024];
-    char method[10], url[1024];
+    char method[10];
     
     fgets(premiere_ligne, 1024, open);
     sscanf(premiere_ligne,"%s %s HTTP/%d.%d", method, request->url, &request->major_version,&request->minor_version);
@@ -72,12 +72,13 @@ int parse_http_request(FILE *open,http_request *request){
         
     if(strcmp(method, "GET")!= 0){
   
-        ok=-1;
+        ok=-2;
           
     }
-    else if(strcmp(url, "/")!= 0){
+    else if(strcmp(request->url, "/") !=0){
     
-        ok=0;
+        printf("url : '%s'\n", request->url);
+        ok=-1;
         
     }
     else if(request->major_version != 1){
@@ -93,7 +94,7 @@ int parse_http_request(FILE *open,http_request *request){
     if(ok == 1){
           /*si ok==1 alors on a forcement la method GET*/
     request->method = HTTP_GET;        
-    }else {
+    }else if(ok == -2){
     request->method = HTTP_UNSUPPORTED;
     }
      
@@ -103,9 +104,11 @@ int parse_http_request(FILE *open,http_request *request){
 void skip_headers(FILE *client){
     char message[1024];
     char *ret = fgets_or_exit(message,1024,client);
-    int n = strlen(ret);
-    while (n > 2){
+
+    while (strlen(ret) > 2 ){
+    
     ret = fgets_or_exit(message,1024,client);
+    
     }
 }
 char *rewrite_url(char *url){
@@ -131,22 +134,19 @@ void send_response(FILE *client , int code ,http_request request, const char *re
 
     /* envoie le corps du message selon les differents cas */
     fprintf(client,"%s",message_body);
+    fflush(client);
 }
 int main(){
     http_request request; 
 	int clientfd, /*retfd,*/socket_serveur ;
 	unsigned int size_client;
-    char sms_client[1024] ;
+   
 	struct sockaddr_in client_addr;
 	
-	/*char *serverName = "TheAnswer";*/
 	
 	char *MESSAGE_BIENVENUE = "Bonjour, Welcome, Bienvenido, Bienvenito, ahlan wa sahlan, vous etes connecté au merveilleux serveur The Answer, tout vos desirs sont des ordres je me ferais un réel plaisir de vous servir et de pouvoir repondre à vos magnifiques questions avec la plus belle facon possible, c'est a dire avec un super smile :D, bonnne navigation et au plaisir de vous revoir tres bientot et tres souvent merveilleux utilisateur que vous etes\n";
 	
-	  /*const char *chaine_erreur= "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad request\r\n";
-    const char *chaine_erreur404= "HTTP/1.1 404 Not found\r\nConnection: close\r\nContent-Length: 15\r\n\r\n404 Not found\r\n";
-      const char* okSMS = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 13\r\n\r\nTout est ok\r\n";
-    */
+	
 	initialiser_signaux();
 	if((socket_serveur=creer_serveur(8080))==-1){
 		perror("pb creer_serveur");
@@ -171,45 +171,21 @@ int main(){
          
 	
 		int bad_request =parse_http_request(open,&request);
-		do{
-            /*utilisation de fgets au lieu de read pour lire une ligne recu du client*/
-		     fgets_or_exit(sms_client, 1024, open);
-            printf("%d %s \n",(int) strlen(sms_client), sms_client);
-		        /* on lui renvoie le message recu par le client 
-
-		         
-			if(fprintf(open, "<%s>  %s",serverName, sms_client)<=0){
-				perror("error perroquet fprintf");
-				return -1;
-			}*/
-		        filterString(sms_client);
-		         printf("%d %s \n",(int) strlen(sms_client), sms_client);
-        } while(strlen(sms_client));
+		skip_headers(open);
         
-			printf("the answer sort de la boucle de lecture");
-			/*if (ok == -1){
-			fprintf(open,"%s",chaine_erreur404);
-			}else{
-			fprintf(open,"%s",MESSAGE_BIENVENUE);
-			}
-			if(ok==0)
-			fprintf(open,"%s",chaine_erreur);
-			if(ok==1)
-			fprintf(open,"%s",okSMS);
-			*/
-			
-			if (bad_request == -1)
+          /* envoie de la reponse au client */
+			if (bad_request == -2)
             send_response(open , 400, request, "Bad Request", "Bad request\r\n");
+             else if (bad_request == -1)
+            send_response(open , 404, request, "Not Found", "Not Found\r\n");
             else if (request.method == HTTP_UNSUPPORTED)
             send_response(open , 405, request, "Method Not Allowed", "Method Not Allowed\r\n");
             else if (strcmp(request.url, "/") == 0)
             send_response(open , 200,request, "OK", MESSAGE_BIENVENUE);
-            else
-            send_response(open , 404, request, "Not Found", "Not Found\r\n");
-		
+           
 
 			
-		/*printf("client deconnecté\n");*/
+		
             exit(0);
 		
 		} else {
